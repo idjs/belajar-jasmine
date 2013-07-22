04cb14d1deab3b8aaba173f2c5b335d936a00806
A. Akbar Hidayat <keripix@gmail.com>
2013-07-22 14:08:35 +0700

    remove log

--- libs/autostrapdown.js
+++ libs/autostrapdown.js
@@ -2,7 +2,6 @@
     var md = document.getElementById("md-content"),
         ajax = global.ajax;
     ajax.get("README.md", function(text){
-        console.log("yoo");
         md.innerText = text;
     });
 })(this);
