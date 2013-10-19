c721a6af66eb328e522eb78ffc42c0b4ddc02873
A. Akbar Hidayat <keripix@gmail.com>
2013-10-19 09:46:14 +0700

    lengkapi

--- code/src/vector2d.js
+++ code/src/vector2d.js
@@ -1,11 +1,19 @@
 (function(global){
     var vector2d = {
-        add: function(v1, v2){
+        add: function( v1, v2 ){
             return {x: v1.x + v2.x , y: v1.y + v2.y};
         },
 
-        substract: function(v1, v2){
+        substract: function( v1, v2 ){
             return {x: v1.x - v2.x , y: v1.y - v2.y};
+        },
+
+        dot: function( v1, v2 ) {
+            return {x: v1.x * v2.x, y: v1.y * v2.y};
+        },
+
+        distance: function( v1, v2 ) {
+            return {};
         }
     };
 
